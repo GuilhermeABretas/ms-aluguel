857d58e90823641cd153df7cd2cc49558371f68a
unknown <guilhermebretas.2003@gmail.com>
2025-12-16 03:31:07 -0300

    fix: final change for full coverage

--- src/main/java/br/com/bicicletario/ms_aluguel/api/dto/AluguelDTO.java
+++ src/main/java/br/com/bicicletario/ms_aluguel/api/dto/AluguelDTO.java
@@ -1,9 +1,19 @@
 package br.com.bicicletario.ms_aluguel.api.dto;
 
 import br.com.bicicletario.ms_aluguel.domain.model.Aluguel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AluguelDTO {
+
     private Long id;
     private Long ciclista;
     private Long bicicleta;
@@ -11,26 +21,20 @@ public class AluguelDTO {
     private LocalDateTime dataHoraDevolucao;
     private Double valorCobrado;
 
+
     public AluguelDTO(Aluguel entidade) {
-        this.id = entidade.getId();
-        this.ciclista = entidade.getCiclista().getId();
-        this.bicicleta = entidade.getIdBicicleta();
-        this.dataHoraRetirada = entidade.getDataHoraRetirada();
-        this.dataHoraDevolucao = entidade.getDataHoraDevolucao();
-        this.valorCobrado = entidade.getValorCobrado();
+        if (entidade != null) {
+            this.id = entidade.getId();
+
+            // Proteção contra NullPointerException caso o ciclista não venha preenchido
+            if (entidade.getCiclista() != null) {
+                this.ciclista = entidade.getCiclista().getId();
+            }
+
+            this.bicicleta = entidade.getIdBicicleta();
+            this.dataHoraRetirada = entidade.getDataHoraRetirada();
+            this.dataHoraDevolucao = entidade.getDataHoraDevolucao();
+            this.valorCobrado = entidade.getValorCobrado();
+        }
     }
-
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public Long getCiclista() { return ciclista; }
-    public void setCiclista(Long ciclista) { this.ciclista = ciclista; }
-    public Long getBicicleta() { return bicicleta; }
-    public void setBicicleta(Long bicicleta) { this.bicicleta = bicicleta; }
-    public LocalDateTime getDataHoraRetirada() { return dataHoraRetirada; }
-    public void setDataHoraRetirada(LocalDateTime dataHoraRetirada) { this.dataHoraRetirada = dataHoraRetirada; }
-    public LocalDateTime getDataHoraDevolucao() { return dataHoraDevolucao; }
-    public void setDataHoraDevolucao(LocalDateTime dataHoraDevolucao) { this.dataHoraDevolucao = dataHoraDevolucao; }
-    public Double getValorCobrado() { return valorCobrado; }
-    public void setValorCobrado(Double valorCobrado) { this.valorCobrado = valorCobrado; }
 }
